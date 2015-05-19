b140ee92506640066703492927ba9310247811d5
shoxxdj <dj.shoxx.dj@gmail.com>
2015-05-19 22:02:42 +0200

    lala

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 //Configuration file
 //Devices is an array with all the docker servers you want to manage
- devices = ['localhost'];
+ devices = ['localhost','192.168.1.22'];
 //Port is the port where this app has to connect to contact the docker service on the device's list
  port = "31337";
 
